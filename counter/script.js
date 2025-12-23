4b47b85787848d4e7dc3a0f653b46e6cee91c6c8
amitesh002 <yadavamitesh44@gmail.com>
2025-12-23 20:41:47 +0530

    changes ui of counter app

--- counter/script.js
+++ counter/script.js
@@ -1,14 +1,16 @@
 let count=0;
 document.getElementById('decrease').addEventListener('click',()=> {
+    if(count>0){
+        count-- ;
+    }
     
-    count--
     document.getElementById('count').innerHTML=`${count}`
 })
 document.getElementById('reset').addEventListener('click',()=> {
     count=0
     document.getElementById('count').innerHTML=`${count}`
 })
-document.getElementById('increase').addEventListener('mouseenter',()=> {
+document.getElementById('increase').addEventListener('click',()=> {
     count++
     document.getElementById('count').innerHTML=`${count}`
 })
